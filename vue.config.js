932d8547b2e2266fcd36a1df6494ff1c9f6ff80f
Bojki <91488932+bojkip@users.noreply.github.com>
2022-08-25 18:09:28 +0200

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  chainWebpack: config => {
+    config.performance
+      .maxEntrypointSize(400000)
+      .maxAssetSize(400000)
+  },
   transpileDependencies: [
     'vuetify',
   ],
